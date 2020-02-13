918bbb542f2eecc96b5f5ff9c6a723457401e72c
YueLiu-ada <59384345+YueLiu-ada@users.noreply.github.com>
2020-02-13 18:12:43 -0500

    Updates

---
+++ precache-manifest.0366ace5f4c56388858722fcae11f7d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a141de78a6f28f51ad0e67949e87a1d",
+    "url": "/jianshu/index.html"
+  },
+  {
+    "revision": "740a307879004f6399af",
+    "url": "/jianshu/static/js/2.ddb6bbe5.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/jianshu/static/js/2.ddb6bbe5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fddcff98ec62838a001",
+    "url": "/jianshu/static/js/3.3d08b21c.chunk.js"
+  },
+  {
+    "revision": "3f2a73c7e075ee01ebc8",
+    "url": "/jianshu/static/js/main.84d0b1db.chunk.js"
+  },
+  {
+    "revision": "1d3502c6fbbf786fe846",
+    "url": "/jianshu/static/js/runtime-main.a1a97a55.js"
+  }
+]);
